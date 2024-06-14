7a8503dd1e6c3d7599dc27dec94bd68859773e22
talalmog8 <talalmog8@gmail.com>
2024-06-14 16:46:12 +0300

    get song middleware

--- SongsAnalyzer/MainWindow.xaml.cs
+++ SongsAnalyzer/MainWindow.xaml.cs
@@ -8,13 +8,13 @@ namespace SongsAnalyzer
     {
         private readonly ISongAnalyzer _songAnalyzer;
         private ObservableCollection<Word> _words;
+        private readonly ObservableCollection<SongComposer> _songComposers = [];
 
         public WindowHandlers()
         {
             InitializeComponent();
             _songAnalyzer = App.Provider.GetRequiredService<ISongAnalyzer>();
             _words = new ObservableCollection<Word>();
-            _songComposers = new ObservableCollection<SongComposer>();
             WordsDataGrid.ItemsSource = _words;
             QuerySongResultsDataGrid.ItemsSource = _songComposers;
         }
@@ -85,7 +85,136 @@ namespace SongsAnalyzer
         {
             MessageBox.Show("Please load a song first", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
+
+
+        #region QuerySong
+
+        private async Task UpdateSongsResultsTable()
+        {
+            // populate  word data grid
+            
+            _songComposers.Clear();
+            
+            var songs = await _songAnalyzer.GetSongs(
+                songName: QuerySongNameTextBox.Text,
+                composerFirstName: QueryComposerFirstNameTextBox.Text,
+                composerLastName: QueryComposerLastNameTextBox.Text,
+                freeText: QueryWordsTextBox.Text);
+           
+            foreach (var song in songs)
+                _songComposers.Add(song);
+
+            QuerySongNameTextBox.Dispatcher.InvokeAsync(() => QuerySongNameTextBox.Clear());
+            QueryComposerFirstNameTextBox.Dispatcher.InvokeAsync(() => QueryComposerFirstNameTextBox.Clear());
+            QueryComposerLastNameTextBox.Dispatcher.InvokeAsync(() => QueryComposerLastNameTextBox.Clear());
+            QueryWordsTextBox.Dispatcher.InvokeAsync(() => QueryWordsTextBox.Clear());
+        }
+
+        private async void QuerySongButton_Click(object sender, RoutedEventArgs e)
+        {
+            await UpdateSongsResultsTable();
+        }
         
+        #endregion
         
+        #region Add Song
+
+        private const string SPACE = " ";
+
+        private void AddComposerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_songAnalyzer.Processed)
+            {
+                SongIsNotProcessed();
+                return;
+            }
+            
+            var composer = Interaction.InputBox("Enter composer name:", "Add Composer", "");
+            if (!string.IsNullOrEmpty(composer) && composer.Split(SPACE, StringSplitOptions.RemoveEmptyEntries).Length > 1) 
+                ComposersListBox.Items.Add(composer);
+            else 
+                FullNameError();
+        }
+
+        private void AddWriterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_songAnalyzer.Processed)
+            {
+                SongIsNotProcessed();
+                return;
+            }
+            
+            var writer = Interaction.InputBox("Enter writer name:", "Add Writer", "");
+            if (!string.IsNullOrEmpty(writer) && writer.Split(SPACE, StringSplitOptions.RemoveEmptyEntries).Length > 1)
+                WritersListBox.Items.Add(writer);
+            else 
+                FullNameError();
+        }
+        
+        private void AddPerformerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_songAnalyzer.Processed)
+            {
+                SongIsNotProcessed();
+                return;
+            }
+            
+            var performer = Interaction.InputBox("Enter performer full name. use a space as a separator:", "Add Performer", "");
+            if (!string.IsNullOrEmpty(performer) && performer.Split(SPACE, StringSplitOptions.RemoveEmptyEntries).Length > 1)
+                PerformersListBox.Items.Add(performer);
+            else 
+                FullNameError();
+        }
+
+        private async void AddSongButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_songAnalyzer.Processed)
+            {
+                SongIsNotProcessed();
+                return;
+            }
+            
+            var composers = new HashSet<Name>();
+            var writers = new HashSet<Name>();
+            var performers = new HashSet<Name>();
+
+            PopulateSet(composers, ComposersListBox.Items);
+            PopulateSet(writers, WritersListBox.Items);
+            PopulateSet(performers, PerformersListBox.Items);
+
+            try
+            {
+                await _songAnalyzer.AddSong(composers, performers, writers);
+                ComposersListBox.Dispatcher.InvokeAsync(() => ComposersListBox.Items.Clear());
+                WritersListBox.Dispatcher.InvokeAsync(() => WritersListBox.Items.Clear());
+                PerformersListBox.Dispatcher.InvokeAsync(() => PerformersListBox.Items.Clear());
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Failed to add song. {exception}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void PopulateSet(HashSet<Name> hashSet, ItemCollection collection)
+        {
+            foreach (var item in collection)
+            {
+                if(item is not null && !string.IsNullOrEmpty(item?.ToString()))
+                {
+                    var fullName = item!.ToString();
+                    var sep = fullName!.IndexOf(SPACE, StringComparison.Ordinal);
+                    var firstName = fullName[..sep].Trim();
+                    var lastName = fullName[(sep + 1)..].Trim();
+                    hashSet.Add(new Name(firstName, lastName));
+                }
+            }
+        }
+
+        private static void FullNameError()
+        {
+            MessageBox.Show("Please use a full name, use a space as a separator", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        
+        #endregion
     }
 }
